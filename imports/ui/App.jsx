b26ef1397b55027744ee6e81662a6358f42d6924
Camilo Alfonso <ca.alfonso967@uniandes.edu.co>
2017-10-08 22:18:43 -0500

    Deployment 1

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -148,7 +148,7 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Poetry </h1>
+                    <h1>Poetry 1.0</h1>
                     <p><i>At the touch of love everyone becomes a poet</i></p>
 
                     <AccountsUIWrapper />
